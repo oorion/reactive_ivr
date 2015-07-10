13b790713ea485f55b1fa42bb4e0bb022ac1713a
Brody Berson <brody.berson@me.com>
2015-07-10 11:08:25 -0600

    Moved Phone Number on top for Transfers

--- assets/js/nodes/transfer_node.js
+++ assets/js/nodes/transfer_node.js
@@ -2,9 +2,9 @@ app.TransferNode = React.createClass({
   render: function() {
     return (
       <div className="content">
-        <app.Prompt prompt={this.props.node.prompt} />
-        <br />
-        Destination: <input className="form-control" type="text" value={this.props.node.destination} />
+      Destination: <input className="form-control" type="text" value={this.props.node.destination} />
+      <br />
+      <app.Prompt prompt={this.props.node.prompt} />
       </div>
     );
   }
